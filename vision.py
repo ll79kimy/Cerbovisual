2ca050aad5a29dd3eda8d479fd94c36c8190c56a
Unknown <Lari-loy@web.de>
2018-05-11 14:59:45 +0000

    Finish

--- vision.py
+++ vision.py
@@ -35,6 +35,19 @@ def binary(img):
 	ret,thresh = cv2.threshold(img,0,255,0)
 	return thresh
 
+def detectColor(img):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	# define range of yellow color in HSV
+	lower_yel = np.array([13,100,100])
+	upper_yel = np.array([20,255,255])
+	
+	# Threshold the HSV image to get only yellow colors
+	colorMask = cv2.inRange(hsv, lower_yel, upper_yel)
+	segment = cv2.cvtColor(colorMask, cv2.COLOR_GRAY2RGB)
+	return segment, np.sum(colorMask/255)
+
+
 rospy.init_node("camera_proves")
 rospy.Subscriber("/camera/rgb/image_color", Image, getRGB)
 rospy.Subscriber("/camera/depth/image", Image, getDepth)
@@ -42,22 +55,20 @@ rospy.Subscriber("/camera/depth/image", Image, getDepth)
 bridge = CvBridge()
 while not rospy.is_shutdown():	
 	if RGB != None and depth != None:
-		
+		segment = None		
 		gray = cv2.cvtColor(RGB,cv2.COLOR_BGR2GRAY)
 		#edges = cv2.Canny(gray,400,400/2,apertureSize = 3)
 		circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,200,  
 param1=40,param2=60,minRadius=0,maxRadius=300)
 			
-		mask = np.zeros(RGB.shape, dtype='uint8')
+		
 		if circles != None and len(circles) > 0:
 			circles = np.uint16(np.around(circles))
 			
-			bestColorEstimator = 0
-			bestCircle = None
-			nPixelCircle = 0
-			bestMask = 0
+		
 			
-			for i in circles[0,:]:				
+			for i in circles[0,:]:
+				mask = np.zeros(RGB.shape, dtype='uint8')				
 				cv2.circle(mask,(i[0],i[1]),i[2],(1,1,1),-1)
 				#mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 				#mask = cv2.resize(mask,(480,360), interpolation = cv2.INTER_LANCZOS4  )
@@ -67,27 +78,41 @@ param1=40,param2=60,minRadius=0,maxRadius=300)
 
 				
 				# Convert BGR to HSV
-			  	hsv = cv2.cvtColor(RGB, cv2.COLOR_BGR2HSV)
+			  	hsv = cv2.cvtColor(detector, cv2.COLOR_BGR2HSV)
 
 				# define range of yellow color in HSV
-				lower_yel = np.array([20,100,100])
-				upper_yel = np.array([30,255,255])
+				lower_yel = np.array([13,100,100])
+				upper_yel = np.array([20,255,255])
 				
 
 				# Threshold the HSV image to get only yellow colors
 				colorMask = cv2.inRange(hsv, lower_yel, upper_yel)
 				nPixelsColor= np.sum (colorMask)/255
-				print(nPixelsColor, nPixelsCircle, nPixelsColor/nPixelsCircle)
 				colorEstimator = nPixelsColor/nPixelsCircle
-				if colorEstimator > bestColorEstimator:
-					bestColorEstimator = colorEstimator
-					bestCircle = i
-					bestMask = mask
-		#print(circles.shape)
+				if colorEstimator > 0.8:
+					segment = mask*RGB	
 			
-		detector = mask*RGB
-		cv2.imshow("Image from my node", colorMask )		
+		if segment == None:
+		#if no circle is detected, turn in direcction of the yellow or left
+	
+			A = np.zeros((RGB.shape[0],int(RGB.shape[1]/2),3), dtype='uint8')	
+			l = np.concatenate((RGB[:,0:int(RGB.shape[1]/2)], A),axis=1)
+			r = np.concatenate((A, RGB[:,int(RGB.shape[1]/2):RGB.shape[1]]),axis=1)
+			segmentl, nPixelsl= detectColor(l)
+			segmentr, nPixelsr= detectColor(r)
+			print(nPixelsr,nPixelsl)
+			if nPixelsl > 1.1*nPixelsr:
+				segment = segmentl
+			elif nPixelsr > 1.1*nPixelsl:
+				segment = segmentr
+			else:
+				segment = RGB
+		#cv2.imshow("images", np.hstack([segmentl, segmentr,segment]))
+		cv2.imshow("Image from my node", segment )		
 		cv2.waitKey(1)
+
+			
+
 
 
 
